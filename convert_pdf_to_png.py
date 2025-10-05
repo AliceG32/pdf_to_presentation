5b5d67bdf2f3aaf3af88b60b9ada071d4c5fa412
alice <aliseklg@gmail.com>
2025-10-05 12:33:30 +0300

    fix converting

--- convert_pdf_to_png.py
+++ convert_pdf_to_png.py
@@ -1,3 +1,4 @@
+import fitz
 from pdf2image import convert_from_path
 from dotenv import load_dotenv
 import os
@@ -6,13 +7,20 @@ load_dotenv()
 
 file_path = os.getenv("FILE_PATH")
 
-output_dir = "test_images"
+doc = fitz.open(file_path)
+
+# узнает размер pdf страницы, чтобы изображение было нужного размера и при поиски блоков текста координаты совпадали
+page = doc[0]
+width = page.rect.width
+height = page.rect.height
+
+output_dir = "test_images_2"
 os.makedirs(output_dir, exist_ok=True)
 
 dpi_list = [100, 200, 300, 400, 500]
 
 for dpi in dpi_list:
-    images = convert_from_path(file_path, dpi=dpi)
+    images = convert_from_path(file_path, dpi=dpi, size=(width, height))
     for i, image in enumerate(images):
         image_name = f"page_{i + 1}_dpi_{dpi}.png"
         image.save(os.path.join(output_dir, image_name), 'PNG')
